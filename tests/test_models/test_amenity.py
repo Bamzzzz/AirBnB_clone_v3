07b9b23898d85f5cda9c3e4b62f23b010cc672fd
Bamzzzz <138697671+Bamzzzz@users.noreply.github.com>
2024-05-27 18:05:00 +0100

    AirBnB_clone_v3

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,31 +1,72 @@
 #!/usr/bin/python3
-"""
-This module instantiates an object of storage engine
-"""
-import os
-from tests.test_models.test_base_model import TestBasemodel
+
+'''
+    All the test for the amenity model are implemented here.
+'''
+
+import unittest
+import pep8
+from models.base_model import BaseModel
 from models.amenity import Amenity
+from os import getenv, remove
 
 
-class TestAmenity(TestBasemodel):
-    """
-    Test the Amenity class and its methods
-    """
+storage = getenv("HBNB_TYPE_STORAGE", "fs")
 
-    def __init__(self, *args, **kwargs):
-        """
-        Initializes the test class for Amenity
-        """
-        super().__init__(*args, **kwargs)
-        self.name = "Amenity"
-        self.value = Amenity
 
-    def test_name2(self):
-        """
-        Tests the type of name attribute
-        """
-        new = self.value()
-        if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-            self.assertNotEqual(type(new.name), str)
-        else:
-            self.assertEqual(type(new.name), type(None))
+class TestAmenity(unittest.TestCase):
+    '''
+        Testing Amenity class
+    '''
+    @classmethod
+    def setUpClass(cls):
+        '''
+            Sets up unittest
+        '''
+        cls.new_amenity = Amenity()
+        cls.new_amenity.name = "wifi"
+
+    @classmethod
+    def tearDownClass(cls):
+        '''
+            Tears down unittest
+        '''
+        del cls.new_amenity
+        try:
+            remove("file.json")
+        except FileNotFoundError:
+            pass
+
+    def test_pep8_style_check(self):
+        '''
+            Tests pep8 style
+        '''
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/amenity.py'])
+        self.assertEqual(p.total_errors, 0, "pep8 error needs fixing")
+
+    def test_States_dbtable(self):
+        '''
+            Check if the tablename is correct
+        '''
+        self.assertEqual(self.new_amenity.__tablename__, "amenities")
+
+    def test_Amenity_inheritence(self):
+        '''
+            tests that the Amenity class Inherits from BaseModel
+        '''
+        self.assertIsInstance(self.new_amenity, BaseModel)
+
+    def test_Amenity_attributes(self):
+        '''
+            Test that Amenity class had name attribute.
+        '''
+        self.assertTrue("name" in self.new_amenity.__dir__())
+
+    @unittest.skipIf(storage == "db", "Testing database storage only")
+    def test_Amenity_attribute_type(self):
+        '''
+            Test that Amenity class had name attribute's type.
+        '''
+        name_value = getattr(self.new_amenity, "name")
+        self.assertIsInstance(name_value, str)
